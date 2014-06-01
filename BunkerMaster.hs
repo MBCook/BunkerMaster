f32f55fb74e70f2814a74646ab227d493b091065
Michael Cook <foobarsoft@foobarsoft.com>
2014-05-31 20:03:38 -0500

    More useful functions

--- BunkerMaster.hs
+++ BunkerMaster.hs
@@ -16,9 +16,9 @@ data Terrain = Terrain {
 					humans :: Bool
 				}
 
-data Point = Point Int Int deriving Eq
-
 data MapSize = MapSize Int Int
+
+type Point = (Int, Int)
 
 type Map = [[Terrain]]
 
@@ -38,11 +38,17 @@ instance Show Terrain where
 		
 ------------------ Functions to work on our types ------------------
 
+-- Turn our map into a displayable string
 showMap :: Map -> String
 showMap m = unlines $ map (\x -> concat $ map show x) m	-- Convert chars with show, join with concat, join with unlines
 
-validPoint :: MapSize -> Point -> Bool	-- Simple bounds check function
-validPoint (MapSize w h) (Point x y)
+-- Determine map size so we don't have to pass it around
+findMapSize :: Map -> MapSize
+findMapSize m@(r:_) = MapSize (length r) (length m)
+
+-- Simple bounds check function
+validPoint :: MapSize -> Point -> Bool
+validPoint (MapSize w h) (x, y)
 	| x <= 0 || y <= 0	= False
 	| x > w || y > h	= False
 	| otherwise			= True
@@ -60,7 +66,7 @@ parseTerrain c = case c of
 -- Given a function to test a terrain type and a row and it's Y index generate the coords of matching squares
 findLocationsInRow :: (TerrainType -> Bool) -> [Terrain] -> Int -> [Point]
 findLocationsInRow f [] _	= []
-findLocationsInRow f r i	= [ Point (x + 1) i | x <- [0,1..(length r)], f . kind $ r !! x ]
+findLocationsInRow f r i	= [ (x + 1, i) | x <- [0,1..(length r)], f . kind $ r !! x ]
 
 -- Find all matching locations in a map. Use zipWith to generate a list of lists of points, concat to join it together
 findLocations :: (TerrainType -> Bool) -> Map -> [Point]
@@ -85,6 +91,15 @@ bugStart = findLocations ((==) Nest)
 
 humanStart :: Map -> [Point]
 humanStart = findLocations ((==) Bunker)
+
+wallLocations :: Map -> [Point]
+wallLocations = findLocations ((==) Wall)
+
+-- Find the neighboring cells
+neighbors :: Map -> Point -> [Point]
+neighbors m (x, y) = [ (x + i, y + j) | i <- [-1,1], j <- [-1,1], validPoint size (x + i, y + j) ]
+	where
+		size = findMapSize m
 
 ------------------ Our main function, to do the work ------------------
 
