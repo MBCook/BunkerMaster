4794e0aca98b5a62cbc8b5f5b0273b55b7b6aca3
Michael Cook <foobarsoft@foobarsoft.com>
2014-05-31 21:10:37 -0500

    Added functions to update the map, filter already visisted squares from a list

--- BunkerMaster.hs
+++ BunkerMaster.hs
@@ -1,6 +1,8 @@
 -- A program to take figure out how to protect humans for horrible termites
 -- Challenge from: http://www.reddit.com/r/dailyprogrammer/comments/26oop1/5282014_challenge_164_intermediate_part_3_protect/
 
+import qualified Data.Set as Set
+
 ------------------ Some types we'll use ------------------
 
 data TerrainType = Nest | Impassible | Unreliable | Reliable | Bunker | Wall deriving Eq
@@ -59,12 +61,22 @@ parseTerrain c = case c of
 
 -- Given a function to test a terrain type and a row and it's Y index generate the coords of matching squares
 findLocationsInRow :: (TerrainType -> Bool) -> [Terrain] -> Int -> [Point]
-findLocationsInRow f [] _	= []
-findLocationsInRow f r i	= [ (x + 1, i) | x <- [0,1..(length r)], f . kind $ r !! x ]
+findLocationsInRow f r i = [ (x + 1, i) | x <- [0,1..(length r)], f . kind $ r !! x ]
 
 -- Find all matching locations in a map. Use zipWith to generate a list of lists of points, concat to join it together
 findLocations :: (TerrainType -> Bool) -> Map -> [Point]
 findLocations f m = concat $ zipWith (findLocationsInRow f) m [1,2..]
+
+-- Get a given piece of terrain
+findTerrain :: Map -> Point -> Terrain
+findTerrain m (x, y) = m !! (y - 1) !! (x - 1)
+
+-- Filter a list of updates to skip places the entity has already visited
+filterVisited :: (Terrain -> Bool) -> Map -> [Point] -> [Point]
+filterVisited f m [] = []
+filterVisited f m (p:ps)
+	| f $ findTerrain m p	= p : filterVisited f m ps
+	| otherwise				= filterVisited f m ps
 
 -- Figure out if we can place a wall on a square
 canWall (Terrain Reliable _ _)	= True
@@ -79,7 +91,7 @@ bugCrossable _				= False
 humanCrossable	Reliable	= True
 humanCrossable _			= False
 
--- Convenience functions to start us out
+-- Convenience functions to find locations
 bugStart :: Map -> [Point]
 bugStart = findLocations ((==) Nest)
 
@@ -114,6 +126,31 @@ humanize t@(Terrain kind buggy humany)
 	| otherwise				= (t, False)								-- Can't cross, don't bother
 	where
 		crossable = humanCrossable kind
+
+-- Update points in the set with the given function collecting coords of updated squares that need continued processing
+updateTerrainRow :: (Terrain -> (Terrain, Bool)) -> Set.Set Point -> [Terrain] -> Int -> ([Terrain], [Point])
+updateTerrainRow f toUpdate t y = foldl helper ([], [])	thatBackwards	-- We'll fold over the row to do the updates
+	where
+		withXPositions	= zip t [1,2..]									-- Convert terrain list to [(Terrain, Int)]
+		thatBackwards	= reverse withXPositions						-- Reverse that because our foldl will reverse it again
+		helper (ts, ps) (t, x)
+			| needsUpdate && keepGoing	= (updated:ts, point:ps)		-- Update terrain, collect point for next update
+			| needsUpdate				= (updated:ts, ps)				-- Update terrain, don't bother collecting point
+			| otherwise					= (ts, ps)						-- No changes
+				where
+					point = (x, y)
+					needsUpdate = Set.member point toUpdate
+					(updated, keepGoing) = f t
+		
+-- Update all the terrain with the given update function, collect the coords of squares that we need to keep working with
+updateTerrain :: (Terrain -> (Terrain, Bool)) -> Set.Set Point -> Map -> (Map, [Point])
+updateTerrain f toUpdate map = foldl helper ([], []) thatBackwards		-- We'll fold over the rows to do the updates
+	where
+		withRowNumbers			= zip map [1,2..]						-- Convert the map to [([Terrain], Int)]
+		thatBackwards			= reverse withRowNumbers				-- Reverse that because our foldl will reverse it again
+		helper (rs, ps) (r, y) = (updatedRow:rs, additionalPoints ++ ps)
+			where
+				(updatedRow, additionalPoints) = updateTerrainRow f toUpdate r y
 
 ------------------ Our main function, to do the work ------------------
 
